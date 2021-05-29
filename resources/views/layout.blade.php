b792f75f88c061ae5b73415fbd8147a0c86952b1
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-05-29 20:04:08 +0100

    removed tawk

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -359,19 +359,6 @@
 <script src="{{ asset('js/countdown.min.js') }}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/608522965eb20e09cf365660/1f440u85h';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
 
 </body>
 
